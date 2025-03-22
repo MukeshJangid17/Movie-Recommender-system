3d3f474abe1216291d913b3ab1b3e3e58838e444
Mukesh <113749367+MukeshJangid17@users.noreply.github.com>
2025-03-22 11:20:01 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    dist = sim[movie_index]
+    movie_list = sorted(list(enumerate(dist)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movie = []
+    for i in movie_list:
+        recommended_movie.append(movies.iloc[i[0]].title)
+    return recommended_movie
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+sim = pickle.load(open('sim.pkl', 'rb'))
+
+st.title("Movies Recommender System")
+
+selected_movie_name = st.selectbox("Select a movie", movies['title'].values)
+
+if st.button('Recommend'):
+    names = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(names[0])
+    
+    with col2:
+        st.text(names[1])
+    
+    with col3:
+        st.text(names[2])
+    
+    with col4:
+        st.text(names[3])
+    
+    with col5:
+        st.text(names[4])
